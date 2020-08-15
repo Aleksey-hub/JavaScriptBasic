153697e08c10c2d59df7c8daf9173e67b9c33c41
Aleksey-hub <53274251+Aleksey-hub@users.noreply.github.com>
2020-08-15 09:32:54 +0500

    Add files via upload

---
+++ task_4.js
@@ -0,0 +1,20 @@
+/*4. Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя параметрами. 
+Т.е. например, функция для сложения должна принимать два числа, складывать их и возвращать результат.
+Обязательно использовать оператор return.*/
+'use strict';
+
+function sum(a, b){
+	return a + b;
+}
+
+function difference(a, b){
+	return a - b;
+}
+
+function division(a, b){
+	return a / b;
+}
+
+function multiplication(a, b){
+	return a * b;
+}
